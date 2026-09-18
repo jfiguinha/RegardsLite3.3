8baa0356ee84503e2e5fd9ce68d53767e23a1ddf
Figuinha <jfiguinha@hotmail.fr>
2026-09-18 18:10:17 +0200

    Update package

--- RegardsViewer_macsilicon/post_process.sh
+++ RegardsViewer_macsilicon/post_process.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-./MacOsLibExplorer ./Release/RegardsLiteViewer.app/Contents/MacOS ./Release/RegardsLiteViewer.app/Contents/Frameworks change_dylib.sh 1
-./MacOsLibExplorer ./Release/RegardsLiteViewer.app/Contents/Frameworks ./Release/RegardsLiteViewer.app/Contents/Frameworks change_dylib.sh 1
-codesign -fs - ./Release/RegardsLiteViewer.app --deep
+./MacOsLibExplorer ./Release/RegardsLite.app/Contents/MacOS ./Release/RegardsLite.app/Contents/Frameworks change_dylib.sh 1
+./MacOsLibExplorer ./Release/RegardsLite.app/Contents/Frameworks ./Release/RegardsLite.app/Contents/Frameworks change_dylib.sh 1
+codesign -fs - ./Release/RegardsLite.app --deep
