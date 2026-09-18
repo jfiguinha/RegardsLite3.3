4e9e57cacd2651c90f4df30435767687310cc5e6
Figuinha <jfiguinha@hotmail.fr>
2026-09-18 18:08:23 +0200

    Update project for linux

--- RegardsViewer_macsilicon/package_create.sh
+++ RegardsViewer_macsilicon/package_create.sh
@@ -1,5 +1,5 @@
 if [ -d package ]; then echo "Exists"; else mkdir package;fi
-cp -r ./Release/RegardsLiteViewer.app ./package/RegardsLiteViewer/
-rm -f ./package/RegardsLiteViewer.dmg
+cp -r ./Release/RegardsLite.app ./package/RegardsLite/
+rm -f ./package/RegardsLite.dmg
 echo "Create Package"
-hdiutil create -fs HFS+ -srcfolder ./package/RegardsLiteViewer -volname RegardsLiteViewer ./package/RegardsLiteViewer.dmg
+hdiutil create -fs HFS+ -srcfolder ./package/RegardsLite -volname RegardsLite ./package/RegardsLite.dmg
