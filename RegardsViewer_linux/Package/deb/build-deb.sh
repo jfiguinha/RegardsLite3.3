3ef106318722ed97c4c40d3312ca398cf38f20d5
Figuinha <jfiguinha@hotmail.fr>
2026-09-21 09:51:31 +0200

    Update

--- RegardsViewer_linux/Package/deb/build-deb.sh
+++ RegardsViewer_linux/Package/deb/build-deb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PACKAGE_DIR="RegardsLite_3.00.0.0-linux-x86_64"
-PACKAGE_NAME="RegardsLite_3.00.0.0_linux_amd64.deb"
+PACKAGE_DIR="RegardsLite_3.00.0.1-linux-x86_64"
+PACKAGE_NAME="RegardsLite_3.00.0.1_linux_amd64.deb"
 
 rm -f "$PACKAGE_NAME"
 
